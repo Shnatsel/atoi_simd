43974bdd3f45b9cbd4a77746e7b6376a94ef10a4
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-08-28 17:03:46 +0200

    fuzz: Also compare the result of the parsing against std

--- fuzz/fuzz_targets/parse_u64.rs
+++ fuzz/fuzz_targets/parse_u64.rs
@@ -3,5 +3,11 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    atoi_simd::parse::<i64>(data);
+    let result = atoi_simd::parse::<u64>(data);
+    if let Ok(number) = result {
+        if let Ok(string) = std::str::from_utf8(data) {
+            let std_number = string.parse::<u64>().expect("atoi_simd parsing succeeded when std failed");
+            assert_eq!(std_number, number);
+        }
+    }
 });

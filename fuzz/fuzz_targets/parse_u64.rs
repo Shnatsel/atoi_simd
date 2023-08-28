b6b77b1c7a4cdc737c974f7cbbc764c1cfc00c40
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-08-28 17:14:57 +0200

    Also use the fuzzer to look for parsing errors

--- fuzz/fuzz_targets/parse_u64.rs
+++ fuzz/fuzz_targets/parse_u64.rs
@@ -4,10 +4,12 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let result = atoi_simd::parse::<u64>(data);
-    if let Ok(number) = result {
-        if let Ok(string) = std::str::from_utf8(data) {
-            let std_number = string.parse::<u64>().expect("atoi_simd parsing succeeded when std failed");
-            assert_eq!(std_number, number);
+    if let Ok(string) = std::str::from_utf8(data) {
+        let std_result = string.parse::<u64>();
+        match (&result, &std_result) {
+            (Ok(simd_num), Ok(std_num)) => assert_eq!(*simd_num, *std_num),
+            (Err(_), Err(_)) => (), // both failed, nothing to do
+            _ => panic!("Mismatch between parsing results! SIMD: {:?}, std: {:?}", &result, &std_result),
         }
     }
 });
